2af72dcb01e33df99b1c44f69ba05074005a1a1c
Gustavo Zanetti <dzanetg@dev.CORP.BTB>
2023-12-20 10:48:48 -0300

    feature: hello world

--- src/main/java/com/training/AngularSpring/AngularSpringApplication.java
+++ src/main/java/com/training/AngularSpring/AngularSpringApplication.java
@@ -8,6 +8,7 @@ public class AngularSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AngularSpringApplication.class, args);
+		System.out.println("Hello world")
 	}
 
 }
